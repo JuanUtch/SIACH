0a72c8fb12a5ecf175320c88945e0bfd3be37a85
IsaacDerEisendrache0 <isaacparraaguirre381@gmail.com>
2024-09-02 19:13:21 -0600

    avance_login

---
+++ src/componentes/login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate(); // Hook para redirección
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        username,
+        password,
+      });
+
+      if (response.data.success) {
+        // Redirige al usuario a la página de "main" después de iniciar sesión
+        navigate('/main');
+      } else {
+        setError('Nombre de usuario o contraseña incorrectos');
+      }
+    } catch (err) {
+      console.error('Error en la solicitud:', err);
+      setError('Error en la solicitud');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Usuario: </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Contraseña:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Iniciar Sesión</button>
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+}
+
+export default Login;
