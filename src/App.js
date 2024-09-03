7540e80c715f86b482b756fea185ded4e55f0769
IsaacDerEisendrache0 <isaacparraaguirre381@gmail.com>
2024-09-02 19:11:36 -0600

    avance_login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Login from './componentes/login'; // Asegúrate de que el componente Login esté correctamente importado
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+        <Routes>
+            <Route path="/" element={<h1 className='text'>Bienvenido a la Página de Inicio</h1>} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          {/* Menú de navegación */}
+          <div className="navbar">
+            <Link to="/">
+              <button className="btn btn-primary">Inicio</button>
+            </Link>
+            <Link to="/login">
+              <button className="btn btn-primary">Iniciar Sesión</button>
+            </Link>
+          </div>
+          {/* Rutas */}
+          
+        </header>
+      </div>
+    </Router>
   );
 }
 
